d5232c1ec90f2911b21d2538773c4a865e1610b0
Mauricio Ruiz <maudiruiz@hotmail.com>
2013-09-23 02:22:41 -0500

    Inicié con device y omniauth gmail

---
+++ app/models/user.rb
@@ -0,0 +1,44 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :uid, :first_name, :last_name, :roles, :domain
+
+  has_and_belongs_to_many :roles
+
+  validates_presence_of :email, :password
+
+	def self.find_for_facebook_oauth(auth, signed_in_resource = nil)
+		user = User.where(:email => auth.info.email).first
+		unless user
+			# CHECK FOR NEW/CREATE
+			user = User.create(first_name:auth.info.first_name, last_name:auth.info.last_name, uid:auth.uid, email:auth.info.email, password:Devise.friendly_token[0,20])
+		end
+		user
+	end
+	def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+		logger.debug access_token.inspect
+	    data = access_token.info
+	    dataraw = access_token.extra.raw_info
+	    user = User.where(:email => data["email"]).first
+
+	    unless user
+	        user = User.create(first_name: data["name"],
+	             email: data["email"],
+	             domain: dataraw["hd"],
+
+	             password: Devise.friendly_token[0,20]
+	            )
+	    end
+	    user
+	end
+
+	def role?(role)
+		return !!self.roles.find_by_name(role)
+	end
+end
